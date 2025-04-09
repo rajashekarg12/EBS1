fb3278ffc20f7157545d4b7110254f542507fac6
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-04-09 07:57:11 +0530

    Update selectAllCountries.sql

--- XXHR/sql/selectAllCountries.sql
+++ XXHR/sql/selectAllCountries.sql
@@ -1,6 +1,8 @@
 --select * from non_existtable;
 --select * from country_table987;
-WHENEVER SQLERROR CONTINUE;
-select * from country_t55;
-select * from country_table987;
-EXIT;
+--WHENEVER SQLERROR CONTINUE;
+--select * from country_t55;
+--select * from country_table987;
+--EXIT;
+
+select * From dual;
